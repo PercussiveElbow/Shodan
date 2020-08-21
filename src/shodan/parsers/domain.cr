2f009676aa508b315c5cca3cc3ac3929649026d6
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2020-08-21 13:48:41 +0100

    Fix for nullable domain

--- src/shodan/parsers/domain.cr
+++ src/shodan/parsers/domain.cr
@@ -1,16 +1,16 @@
 module Shodan
   struct Domain
     include JSON::Serializable
-    property domain : String?
-    property tags : Array(String)?
-    property data : Array(DomainData)?
+    property domain : String
+    property tags : Array(String)
+    property data : Array(DomainData)
   end
 
   struct DomainData
     include JSON::Serializable
-    property subdomain : String?
-    property type : String?
-    property value : String?
-    property last_seen : String?
+    property subdomain : String
+    property type : String
+    property value : String
+    property last_seen : String
   end
 end
