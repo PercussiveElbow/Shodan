df78aba34b13fe5249ee159673d8e72a0dba789e
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2020-11-26 21:01:30 +0000

    SSL schema fix

--- src/shodan/parsers/host_services/ssl.cr
+++ src/shodan/parsers/host_services/ssl.cr
@@ -2,13 +2,40 @@ module Shodan
   struct HostDataSSL
     include JSON::Serializable
     property versions : Array(String)?
-    property acceptable_cas : Array(String)?
+    property acceptable_cas : Array(SslAcceptableCA)
     property chain : Array(String)?
     property alpn : Array(String)?
     property cert : HostDataSSLCert?
     property dhparams : HostDataSSLDHParams?
     property cipher : HostDataSSLCipher?
     property tlssext : Array(HostDataSSLTlsext)?
+  end
+
+  struct SslAcceptableCA 
+    include JSON::Serializable
+    property components : SslCertComponents
+    property hash : Int32
+    property raw : String
+  end
+
+  struct SslCertComponents 
+    include JSON::Serializable
+    @[JSON::Field(key: "C")]
+    property c : String? 
+    @[JSON::Field(key: "CN")]
+    property c : String?    
+    @[JSON::Field(key: "DC")]
+    property dc : String?
+    @[JSON::Field(key: "L")]
+    property l : String?        
+    @[JSON::Field(key: "OU")]
+    property ou : String?
+    @[JSON::Field(key: "SN")]
+    property sn : String?
+    @[JSON::Field(key: "ST")]
+    property st : String?
+    property emailAddress : String? 
+    property serialNumber : String?
   end
 
   struct HostDataSSLDHParams
@@ -18,6 +45,12 @@ module Shodan
     property bits : Int32?
     property generator : Int64?
     property fingerprint : String?
+  end
+
+  struct HostDataPubKey 
+    include JSON::Serializable
+    property bits : Int32 
+    property type : String
   end
 
   struct HostDataSSLTlsext
@@ -36,7 +69,35 @@ module Shodan
     property extensions : Array(HostDataSSLExtensions)?
     property fingerprint : Hash(String, String)?
     property serial : Int64?
+    property subject : HostDataSslSubject
+    property issuer : HostDataSslIssuer
   end
+
+  struct HostDataSslSubject
+    include JSON::Serializable
+    property businessCategory : String? 
+    property description : String? 
+    property jurisdictionC : String? 
+    property jurisdictionST : String? 
+    property postalCode : String? 
+    property street : String? 
+  end
+
+  struct HostDataSslIssuer
+    include JSON::Serializable
+    property name : String?
+    @[JSON::Field(key: "UID")]
+    property uid : String?
+    property dnQualifier : String?
+    property subjectAltName : String?
+    property unstructuredName : String?
+    property unstructuredAddress : String?
+    property postalCode : String?
+    property street : String?
+    @[JSON::Field(key: "UNDEF")]
+    property undef : String?
+  end
+  
 
   struct HostDataSSLCipher
     include JSON::Serializable
